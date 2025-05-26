45b905ca3edb69226392f897b7d8776f69bcd4fa
Ghostosmoke <128992327+Ghostosmoke@users.noreply.github.com>
2025-05-26 06:57:03 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,7 +1,7 @@
 from functools import reduce
 from operator import mul
 from math import factorial
-
+import numpy as np
 
 def R(n: float , T_0: float , lambda_list: float):
     return 1
@@ -33,10 +33,29 @@ def AverageDelayUnregulatedCross(lambda_0: float , T_0: float , lambda_: list, k
     return W_H
 
 def main():
+    print('''Ввод данных происходит по часовой стрелке начиная с дороги которая находится сверху''')
+    kol = int(input('Введите количество полос, где крестообразный = 4, T-образный = 3'))
+    line_T = None
+    dict_number={
+        1:'up',
+        2:'right',
+        3:'down',
+        4:'left'
+    }
+    if kol == 3:
+        print('Вы введи Т - образный перекресток. ')
+        lint_T = str(input('Укажите какой полосы не будет из этих четырех вариантов(up,right,down,left)'))
+
+    for i in range(kol):
+        print('Ввод данных по количеству полос')
+        if dict_number[i] != line_T:
+            print('Вы вводите данные для',dict_number[i])
+
     m_z=AverageWaitingTime(k=2,)
     alf=m_z * lambda_[1] / k[1]
     if alf<1:
         W_H =
+    else:
+        alf=0.9
 if __name__ == "__main__":
     main()
-
